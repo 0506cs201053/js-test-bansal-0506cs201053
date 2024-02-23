e06e17d349fbf2eb7fd4362abf7b60ffca8ae18a
Suraj Prasad Ahirwar <140588730+0506cs201053@users.noreply.github.com>
2024-02-23 13:45:42 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,25 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
-
-module.exports = { isValid };
-
-
+function isValid(s) {
+    const stack = [];
+    const opening = ['(', '{', '['];
+    const closing = [')', '}', ']'];
+  
+    for (let i = 0; i < s.length; i++) {
+      if (opening.includes(s[i])) {
+        stack.push(s[i]);
+      } else if (closing.includes(s[i])) {
+        const lastOpening = stack.pop();
+        if (
+          (s[i] === ')' && lastOpening !== '(') ||
+          (s[i] === '}' && lastOpening !== '{') ||
+          (s[i] === ']' && lastOpening !== '[')
+        ) {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  }
+  console.log(isValid("^{'}"));
+  console.log(isValid(":=^{-}000^{-}"));
+  console.log(isValid("^{\\neg}O^{\\circ}"));
